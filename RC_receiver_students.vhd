05333b62162ef460f7c58eb86070776bdd3d0acd
Mason Haynes <haynesm3@udayton.edu>
2022-12-14 14:38:08 -0500

    Fix data counter
    I fixed the quartus issue with the data counter

--- RC_receiver_students.vhd
+++ RC_receiver_students.vhd
@@ -247,23 +247,19 @@ begin
 		if(rising_edge(clk)) then
 			if((reset = '0') or (data_counter = max_bits-1)) then
 				data_counter <= 0;
+			else--if(rising_edge(reading_data)) then
+				data_counter <= data_counter + 1;
 			end if;
-		elsif (rising_edge(reading_data)) then
-			data_counter <= data_counter + 1;
+		--elsif (rising_edge(reading_data)) then
+			--data_counter <= data_counter + 1;
 		end if;
 	end process data_counter_proc;
 	
 	
-	shift_reg_proc : process(clk, reading_data)
+	shift_reg_proc : process(reading_data)
 	begin
-		if(rising_edge(clk)) then
-			if(reset = '0') then
-				shift_reg <= (others => '0');
-			end if;
-		elsif(rising_edge(reading_data)) then
+		if(rising_edge(reading_data)) then
 			shift_reg <= shift_reg((max_bits-1) - 1 downto 0) & data_bit;
 		end if;
-		-- process to define the shift register that holds the incomming
-		-- data.  (hint:  don't use canned VHDL functions for shifting)
 	end process shift_reg_proc;
 end behavior;	
